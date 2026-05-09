8ac2b323b35698585005f51601a4f1e4f1623dd1
abenezer nebiyu <abenezernebiyu500@gmail.com>
2026-05-09 10:31:02 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
+import java.util.Scanner;//well this is for askin user tf they wanted
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         GamingLaptop myLaptop = new GamingLaptop("MSI", "GF Thin", 16, "GTX 1650");
-
+//i fuckin love my pc
         myLaptop.powerOn();
         myLaptop.showSpecs();
 
@@ -13,6 +13,7 @@ public class Main {
         myLaptop.login(scanner);
 
         System.out.println("\nPress Enter to power off...");
+        //well idk why i even used \n it feels right and it worked there
         scanner.nextLine();
 
         myLaptop.powerOff();
